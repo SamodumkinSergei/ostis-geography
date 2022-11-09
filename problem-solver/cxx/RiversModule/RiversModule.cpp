687ab32624d2d26e477436101324f8a6d9365926
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2022-11-09 22:18:00 +0300

    Remove comments

--- problem-solver/cxx/RiversModule/RiversModule.cpp
+++ problem-solver/cxx/RiversModule/RiversModule.cpp
@@ -4,21 +4,21 @@ SC_IMPLEMENT_MODULE(RiversModule)
 
 sc_result RiversModule::InitializeImpl()
 {
-  if (!Keynodes::InitGlobal()) // exampleModule это название пространства в котором находятся агенты и другие классы которые используются для работы агентов
+  if (!Keynodes::InitGlobal())
   {
     return SC_RESULT_ERROR;
   }
 
-  ScMemoryContext ctx(sc_access_lvl_make_min, "RiversModule"); // тут exampleModule может быть любой строкой, но для простоты исползуется название пространства
+  ScMemoryContext ctx(sc_access_lvl_make_min, "RiversModule");
 
-  SC_AGENT_REGISTER(LongestInRegion) // регистрируем свои агенты, можно сколько угодно агентов зарегистрировать, если этого не сдлеать агент работать не будет, так как не загружен в память.
+  SC_AGENT_REGISTER(LongestInRegion)
 
   return SC_RESULT_OK;
 }
 
 sc_result RiversModule::ShutdownImpl()
 {
-  SC_AGENT_UNREGISTER(LongestInRegion) // по аналогии с регистрацией агентов в памяти, убираем их из памяти, прописать для каждого агента в модуле
+  SC_AGENT_UNREGISTER(LongestInRegion)
 
   return SC_RESULT_OK;
 }
--- problem-solver/cxx/RiversModule/RiversModule.hpp
+++ problem-solver/cxx/RiversModule/RiversModule.hpp
@@ -3,15 +3,14 @@
 #include "sc-memory/sc_memory.hpp"
 #include "sc-memory/sc_module.hpp"
 
-//подключаем все файлы которые используются для работы модуля
 #include "keynodes/keynodes.hpp"
 #include "agents/LongestInRegion.hpp"
 
-#include "RiversModule.generated.hpp" // заменяем тут назавние файла т.е. заменить ExampleModule на свое название
+#include "RiversModule.generated.hpp"
 
 namespace RiversModule {
 
-class RiversModule : public ScModule // название модуля может быть любым (модуль может содержать несколько агентов)
+class RiversModule : public ScModule
 {
   SC_CLASS(LoadOrder(100))
   SC_GENERATED_BODY()
