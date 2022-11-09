37c38b298cc5b6e65bf86d647e96b485c204e733
Pavel-Belous <pavel.belous2002@gmail.com>
2022-11-09 10:40:19 -0800

    feat(problem-solver): AdminModule

---
+++ problem-solver/cxx/AdminModule/AdminModule.cpp
@@ -0,0 +1,25 @@
+#include "AdminModule.hpp"
+
+SC_IMPLEMENT_MODULE(AdminModule)
+
+sc_result AdminModule::InitializeImpl()
+{
+  if (!adminModule::AdminKeynodes::InitGlobal()) // AdminModule это название пространства в котором находятся агенты и другие классы которые используются для работы агентов
+  {
+    return SC_RESULT_ERROR;
+  }
+
+  ScMemoryContext ctx(sc_access_lvl_make_min, "adminModule"); // тут AdminModule может быть любой строкой, но для простоты исползуется название пространства
+  SC_AGENT_REGISTER(adminModule::GetAdminBuildingRegion)
+   // регистрируем свои агенты, можно сколько угодно агентов зарегистрировать, если этого не сдлеать агент работать не будет, так как не загружен в память.
+
+  return SC_RESULT_OK;
+}
+
+sc_result AdminModule::ShutdownImpl()
+{
+  SC_AGENT_UNREGISTER(adminModule::GetAdminBuildingRegion)
+   // по аналогии с регистрацией агентов в памяти, убираем их из памяти, прописать для каждого агента в модуле
+
+  return SC_RESULT_OK;
+}
