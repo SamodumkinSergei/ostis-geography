5d9968167538ce83ee2189ce908d5c835029ffed
garden-rgb <en170901@gmail.com>
2022-11-10 03:37:19 +0300

    feat(problem-solver):agent of search shop Sosodi by district and status

---
+++ problem-solver/cxx/StatusAndDistrictSearchModule/StatusAndDistrictSearchModule/StatusAndDistrictSearchModule.cpp
@@ -0,0 +1,31 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
+#include "StatusAndDistrictSearchModule.hpp"
+namespace StatusAndDistrictSearchModule {
+    SC_IMPLEMENT_MODULE(StatusAndDistrictSearchModule)
+
+        sc_result StatusSearchModule::InitializeImpl()
+        {
+            if (!Keynodes::InitGlobal()) 
+            {
+            return SC_RESULT_ERROR;
+            }
+
+            ScMemoryContext ctx(sc_access_lvl_make_min, "StatusAndDistrictSearchModule");
+
+            SC_AGENT_REGISTER(StatusAndDistrictSearch)
+
+            return SC_RESULT_OK;
+        }
+
+    sc_result RiversModule::ShutdownImpl()
+    {
+        SC_AGENT_UNREGISTER(StatusAndDistrictSearch)
+
+            return SC_RESULT_OK;
+    }
+}
---
+++ problem-solver/cxx/StatusAndDistrictSearchModule/keynodes/keynodes.cpp
@@ -0,0 +1,17 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
+#include "keynodes.hpp"
+
+namespace StatusAndDistrictSearchModule
+{
+	ScAddr Keynodes::action_statusAndDistrictSearch;
+	ScAddr Keynodes::nrel_main_idtf;
+	ScAddr Keynodes::nrel_district;
+	ScAddr Keynodes::nrel_search_area;
+	ScAddr Keynodes::nrel_region;
+	ScAddr Keynodes::nrel_status;
+}
