e0e19c706b8eb3b5d16c7fe6d48fc3f2e5b5f93e
Vaclav Gordienko <vaclavgordienko@gmail.com>
2022-11-11 00:08:31 +0300

    feat(problem-solver): add SearchDanceStudiosByTypeAgent agent

---
+++ problem-solver/cxx/dance_studiosModule/DanceStudiosModule.cpp
@@ -0,0 +1,36 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
+#include "DanceStudiosModule.hpp"
+
+
+#include "agent/SearchDanceStudiosByTypeAgent.hpp"
+#include "keynodes/DanceStudiosKeynodes.hpp"
+
+using namespace dance_studios;
+
+SC_IMPLEMENT_MODULE(DanceStudiosModule)
+
+sc_result DanceStudiosModule::InitializeImpl()
+{
+  SC_LOG_INFO("module start");
+
+  if (!DanceStudiosKeynodes::InitGlobal())
+    return SC_RESULT_ERROR;
+
+
+  SC_AGENT_REGISTER(SearchDanceStudiosByTypeAgent)
+
+  return SC_RESULT_OK;
+}
+
+sc_result DanceStudiosModule::ShutdownImpl()
+{
+
+  SC_AGENT_UNREGISTER(SearchDanceStudiosByTypeAgent)
+
+  return SC_RESULT_OK;
+}
