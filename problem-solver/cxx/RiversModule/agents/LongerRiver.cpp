f91f192a28be92af4db63241b5b7527181980fd9
Savkov Danila <savoooook@gmail.com>
2022-11-10 21:46:35 +0300

    Update and refactor agent

--- problem-solver/cxx/RiversModule/agents/LongerRiver.cpp
+++ problem-solver/cxx/RiversModule/agents/LongerRiver.cpp
@@ -23,9 +23,11 @@ namespace RiversModule
 
 	SC_AGENT_IMPLEMENTATION(LongerRiver)
 	{
-
 		if (!edgeAddr.IsValid())
+		{
+			SC_LOG_ERROR("Initial edge is not valid");
 			return SC_RESULT_ERROR;
+		}
 
 		SC_LOG_INFO("----------LongerRiver begin----------");
 		ScAddr actionNode = ms_context->GetEdgeTarget(edgeAddr);
@@ -35,37 +37,102 @@ namespace RiversModule
 			actionNode,
 			scAgentsCommon::CoreKeynodes::rrel_1);
 
+		if (!river1.IsValid())
+		{
+			SC_LOG_ERROR("First river node is not valid");
+			return SC_RESULT_ERROR_INVALID_PARAMS;
+		}
+		else
+		{
+			std::string river1_idtf = CommonUtils::getIdtf(ms_context.get(), river1, Keynodes::nrel_main_idtf);
+			SC_LOG_INFO("First river main identifier: " + river1_idtf);
+		}
+
 		ScAddr river2 = IteratorUtils::getFirstByOutRelation(
 			&m_memoryCtx,
 			actionNode,
 			scAgentsCommon::CoreKeynodes::rrel_2);
-		ScAddr answer = ms_context->CreateNode(ScType::NodeConstStruct);
 
-		ScIterator5Ptr it = ms_context->Iterator5(river1, ScType::EdgeDCommonConst, ScType::Unknown, ScType::EdgeAccessConstPosPerm, Keynodes::nrel_length);
-		int l1 = 0;
-		while (it->Next())
+		if (!river2.IsValid())
 		{
-			ScAddr len = it->Get(2);
-			std::string str1 = CommonUtils::getIdtfValue(ms_context.get(), len, Keynodes::nrel_main_idtf);
-			l1 = std::atoi(str1.c_str());
-		}
-
-		ScIterator5Ptr it1 = ms_context->Iterator5(river2, ScType::EdgeDCommonConst, ScType::Unknown, ScType::EdgeAccessConstPosPerm, Keynodes::nrel_length);
-		int l2 = 0;
-		while (it1->Next())
-		{
-			ScAddr len = it1->Get(2);
-			std::string str2 = CommonUtils::getIdtfValue(ms_context.get(), len, Keynodes::nrel_main_idtf);
-			l2 = std::atoi(str2.c_str());
-		}
-
-		if (l1 > l2)
-		{
-			ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, river1);
+			SC_LOG_ERROR("Second river node is not valid");
+			return SC_RESULT_ERROR_INVALID_PARAMS;
 		}
 		else
 		{
-			ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, river2);
+			std::string river2_idtf = CommonUtils::getIdtf(ms_context.get(), river2, Keynodes::nrel_main_idtf);
+			SC_LOG_INFO("Second river main identifier: " + river2_idtf);
+		}
+
+		ScAddr answer = ms_context->CreateNode(ScType::NodeConstStruct);
+
+		ScIterator5Ptr river1_iterator = ms_context->Iterator5(
+			river1,
+			ScType::EdgeDCommonConst,
+			ScType::Unknown,
+			ScType::EdgeAccessConstPosPerm,
+			Keynodes::nrel_length);
+		int river1_length = 0;
+		while (river1_iterator->Next())
+		{
+			ScAddr len = river1_iterator->Get(2);
+			std::string str = CommonUtils::getIdtfValue(ms_context.get(), len, Keynodes::nrel_main_idtf);
+			river1_length = std::atoi(str.c_str());
+			SC_LOG_INFO("First river length: " + str);
+		}
+
+		ScIterator5Ptr river2_iterator = ms_context->Iterator5(
+			river2,
+			ScType::EdgeDCommonConst,
+			ScType::Unknown,
+			ScType::EdgeAccessConstPosPerm,
+			Keynodes::nrel_length);
+		int river2_length = 0;
+		while (river2_iterator->Next())
+		{
+			ScAddr len = river2_iterator->Get(2);
+			std::string str = CommonUtils::getIdtfValue(ms_context.get(), len, Keynodes::nrel_main_idtf);
+			river2_length = std::atoi(str.c_str());
+			SC_LOG_INFO("Second river length: " + str);
+		}
+
+		if (river1_length > river2_length)
+		{
+			SC_LOG_INFO("First river is longer");
+			ScIterator5Ptr iteratorToAddToAnswer = ms_context->Iterator5(
+				river1,
+				ScType::Unknown,
+				ScType::Unknown,
+				ScType::EdgeAccessConstPosPerm,
+				Keynodes::nrel_length);
+
+			if (iteratorToAddToAnswer->Next())
+			{
+				ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, iteratorToAddToAnswer->Get(0));
+				ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, iteratorToAddToAnswer->Get(1));
+				ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, iteratorToAddToAnswer->Get(2));
+				ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, iteratorToAddToAnswer->Get(3));
+				ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, iteratorToAddToAnswer->Get(4));
+			}
+		}
+		else
+		{
+			SC_LOG_INFO("Second river is longer");
+			ScIterator5Ptr iteratorToAddToAnswer = ms_context->Iterator5(
+				river2,
+				ScType::Unknown,
+				ScType::Unknown,
+				ScType::EdgeAccessConstPosPerm,
+				Keynodes::nrel_length);
+
+			if (iteratorToAddToAnswer->Next())
+			{
+				ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, iteratorToAddToAnswer->Get(0));
+				ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, iteratorToAddToAnswer->Get(1));
+				ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, iteratorToAddToAnswer->Get(2));
+				ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, iteratorToAddToAnswer->Get(3));
+				ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, iteratorToAddToAnswer->Get(4));
+			}
 		}
 
 		SC_LOG_INFO("----------LongerRiver end----------");
