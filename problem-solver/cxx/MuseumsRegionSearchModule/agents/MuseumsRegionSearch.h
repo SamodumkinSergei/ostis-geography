c58cd3e14456113aba4fefbedc8a6a94b98f6d27
Antony <56368902+Dartosh@users.noreply.github.com>
2022-11-11 22:44:35 +0300

    feat(problem-solver): implement MuseumsRegionSearch agent (#108)
    * refactor: update cmakelists imports
    
    * feat: create main keynodes for agent
    
    * feat: implement search agents
    
    Co-authored-by: Antony Klintsov <antonyklintsov@MacBook-Air-Antony.local>

---
+++ problem-solver/cxx/MuseumsRegionSearchModule/agents/MuseumsRegionSearch.h
@@ -0,0 +1,22 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
+#pragma once
+#include "sc-memory/kpm/sc_agent.hpp"
+
+#include "keynodes/keynodes.hpp"
+#include "MuseumsRegionSearch.generated.hpp"
+
+namespace MuseumsRegionSearchModule
+{
+
+	class MuseumsRegionSearch : public ScAgent
+	{
+		SC_CLASS(Agent, Event(Keynodes::action_museumsRegionSearch, ScEvent::Type::AddOutputEdge))
+			SC_GENERATED_BODY()
+	};
+
+}
