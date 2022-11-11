fa28f21f6a8ec6551dd57e58d416d441860fc8ef
Belous-Pavel <74500097+Belous-Pavel@users.noreply.github.com>
2022-11-11 06:37:25 -0800

    feat(problem-solver): AdminModule (#77)
    * feat(kb): Minsk Villages
    
    * feat(problem-solver): GetVillagesByRegionAgent
    
    * feat(problem-solver): AdminModule
    
    * Delete problem-solver/py/modules/villages_module directory

---
+++ problem-solver/cxx/AdminModule/keynodes/AdminKeynodes.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "sc-memory/sc_addr.hpp"
+#include "sc-memory/sc_object.hpp"
+
+#include "AdminKeynodes.generated.hpp"
+
+namespace adminModule
+{
+
+class AdminKeynodes : public ScObject 
+{
+  SC_CLASS()
+  SC_GENERATED_BODY()
+
+public:
+
+  SC_PROPERTY(Keynode("action_get_embassy"), ForceCreate)
+  static ScAddr action_get_embassy;
+
+  SC_PROPERTY(Keynode("action_get_admin_building_district"), ForceCreate)
+  static ScAddr action_get_admin_building_district;
+
+  SC_PROPERTY(Keynode("concept_admin_building"), ForceCreate)
+  static ScAddr concept_admin_building;
+
+  SC_PROPERTY(Keynode("action_get_admin_building_region"), ForceCreate)
+  static ScAddr action_get_admin_building_region;
+
+  
+  SC_PROPERTY(Keynode("nrel_region"), ForceCreate)
+  static ScAddr nrel_region;
+
+  SC_PROPERTY(Keynode("nrel_embassy"), ForceCreate)
+  static ScAddr nrel_embassy;
+
+  SC_PROPERTY(Keynode("nrel_search_area"), ForceCreate)
+  static ScAddr nrel_search_area;
+
+  SC_PROPERTY(Keynode("minsk"), ForceCreate)
+  static ScAddr minsk;
+
+};
+
+}
