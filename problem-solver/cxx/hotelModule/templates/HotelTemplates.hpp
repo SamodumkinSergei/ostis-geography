3e2fe35a3ef1954732ec130b95b133adbbee43f5
sova <solovyev768@gmail.com>
2022-11-11 22:29:03 +0300

    add GetHotelByMinPricePerNightAgent

---
+++ problem-solver/cxx/hotelModule/templates/HotelTemplates.hpp
@@ -0,0 +1,19 @@
+/*
+ * Author Artsiom Salauyou
+ */
+
+#pragma once
+
+#include <sc-memory/sc_addr.hpp>
+#include <sc-memory/sc_memory.hpp>
+
+namespace hotelModule {
+    class HotelTemplates {
+    public:
+        static std::string const HOTEL_ALIAS;
+
+        explicit HotelTemplates();
+
+        static std::unique_ptr<ScTemplate> getHotelByMinPricePerNightTemplate(ScAddr const & minPricePerNightAddr);
+    };
+}
