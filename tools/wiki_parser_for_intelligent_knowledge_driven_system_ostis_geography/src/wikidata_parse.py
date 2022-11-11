24a9bed498b3feec8374efdf89cd1f62520bdcb3
Pawel Jan Zlomaniec <57328770+straightth@users.noreply.github.com>
2022-11-11 21:46:56 +0300

    feat(tools): adding wiki parser (#114)

---
+++ tools/wiki_parser_for_intelligent_knowledge_driven_system_ostis_geography/src/wikidata_parse.py
@@ -0,0 +1,28 @@
+import argparse
+
+from loaders.WikiDataWithContextLoader import WikiDataWithContextLoader
+from translators.JsonToScsTranslator import json_to_scs
+
+
+def parse(entities, save_dir='sc_out'):
+    loader = WikiDataWithContextLoader()
+    loader.getEntities(entities)
+    json_to_scs(loader.getJson(), save_dir)
+
+def args_parser_init():
+    parser = argparse.ArgumentParser(
+        description='Module for parsing external sources into SC-code')
+    parser.add_argument('entities', metavar='ENT', type=str,
+                        nargs='+', help="Searched entities")
+    parser.add_argument('--dir', default='sc_out', type=str,
+                        help='Enter directory to save scs files')
+    return parser
+
+
+if __name__ == "__main__":
+    parser = args_parser_init()
+    args = parser.parse_args()
+    parse(args.entities, args.dir)
+
+
+
