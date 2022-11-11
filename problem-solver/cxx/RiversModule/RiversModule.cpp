b174e8dee2d9b761dcb8e4b4829d9d3010841564
unvisiblee <bobawot@yandex.ru>
2022-11-11 21:34:31 +0400

    Rivers agents added

---
+++ problem-solver/cxx/RiversModule/RiversModule.cpp
@@ -0,0 +1,29 @@
+#include "RiversModule.hpp"
+namespace RiversModule {
+SC_IMPLEMENT_MODULE(RiversModule)
+
+sc_result RiversModule::InitializeImpl()
+{
+  if (!Keynodes::InitGlobal()) // exampleModule это название пространства в котором находятся агенты и другие классы которые используются для работы агентов
+  {
+    return SC_RESULT_ERROR;
+  }
+
+  ScMemoryContext ctx(sc_access_lvl_make_min, "RiversModule"); // тут exampleModule может быть любой строкой, но для простоты исползуется название пространства
+
+  SC_AGENT_REGISTER(LongestInRegion)
+  SC_AGENT_REGISTER(BiggerBasin)
+  SC_AGENT_REGISTER(LongerRiver) // регистрируем свои агенты, можно сколько угодно агентов зарегистрировать, если этого не сдлеать агент работать не будет, так как не загружен в память.
+
+  return SC_RESULT_OK;
+}
+
+sc_result RiversModule::ShutdownImpl()
+{
+  SC_AGENT_UNREGISTER(LongestInRegion)
+  SC_AGENT_UNREGISTER(BiggerBasin)
+  SC_AGENT_UNREGISTER(LongerRiver) // по аналогии с регистрацией агентов в памяти, убираем их из памяти, прописать для каждого агента в модуле
+
+  return SC_RESULT_OK;
+}
+}
---
+++ problem-solver/cxx/RiversModule/agents/LongerRiver.cpp
@@ -0,0 +1,78 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
+#include "sc-agents-common/utils/AgentUtils.hpp"
+#include "sc-agents-common/utils/CommonUtils.hpp"
+#include "sc-agents-common/utils/IteratorUtils.hpp"
+#include "sc-agents-common/keynodes/coreKeynodes.hpp"
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include "LongerRiver.hpp"
+#include "keynodes/keynodes.hpp"
+
+using namespace std;
+using namespace utils;
+
+namespace RiversModule {
+
+        SC_AGENT_IMPLEMENTATION(LongerRiver)
+	{
+
+if (!edgeAddr.IsValid())
+			return SC_RESULT_ERROR;
+
+
+		SC_LOG_INFO("----------LongerRiver begin----------");
+		ScAddr actionNode = ms_context->GetEdgeTarget(edgeAddr);
+
+ScAddr river1 = IteratorUtils::getFirstByOutRelation(
+        &m_memoryCtx,
+        actionNode,
+        scAgentsCommon::CoreKeynodes::rrel_1); 
+
+ScAddr river2 = IteratorUtils::getFirstByOutRelation(
+        &m_memoryCtx,
+        actionNode,
+        scAgentsCommon::CoreKeynodes::rrel_2);
+ScAddr answer = ms_context->CreateNode(ScType::NodeConstStruct);
+
+
+ScIterator5Ptr it = ms_context->Iterator5(river1, ScType::EdgeDCommonConst,ScType::Unknown, ScType::EdgeAccessConstPosPerm,Keynodes::nrel_length);
+int l1=0;
+while (it->Next())
+{
+ScAddr len = it->Get(2);
+std::string str1 = CommonUtils::getIdtfValue(ms_context.get(), len, Keynodes::nrel_main_idtf);
+l1 = std::atoi(str1.c_str());
+}
+
+ScIterator5Ptr it1 = ms_context->Iterator5(river2, ScType::EdgeDCommonConst,ScType::Unknown, ScType::EdgeAccessConstPosPerm,Keynodes::nrel_length);
+int l2=0;
+while (it1->Next())
+{
+ScAddr len = it1->Get(2);
+std::string str2 = CommonUtils::getIdtfValue(ms_context.get(), len, Keynodes::nrel_main_idtf);
+l2 = std::atoi(str2.c_str());
+}
+
+if(l1>l2)
+{
+ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, river1);
+}
+else
+{
+ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, river2);
+}
+
+		
+		SC_LOG_INFO("----------LongerRiver end----------");
+		AgentUtils::finishAgentWork(ms_context.get(), actionNode, answer);
+		return SC_RESULT_OK;
+	}
+
+}
